4d12290f8050f7c73ba6d6766625e2a688dfac8e
Swarnim Gupta <swarnimgupta2000@gmail.com>
2020-10-01 20:33:55 +0530

    Resolved time format issue

--- src/card.js
+++ src/card.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { MdShoppingCart } from 'react-icons/md';
-import { GiMilkCarton } from 'react-icons/gi'
-import { FaMobileAlt } from 'react-icons/fa'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { MdShoppingCart } from "react-icons/md";
+import { GiMilkCarton } from "react-icons/gi";
+import { FaMobileAlt } from "react-icons/fa";
 
 const useStyles = makeStyles({
   root: {
@@ -22,7 +22,11 @@ const useStyles = makeStyles({
 export default function MediaCard(props) {
   const classes = useStyles();
   return (
-    <Card className={classes.root} id="carde" style={{backgroundColor:"#80ffaa"}}>
+    <Card
+      className={classes.root}
+      id="carde"
+      style={{ backgroundColor: "#80ffaa" }}
+    >
       <CardActionArea>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
@@ -34,8 +38,11 @@ export default function MediaCard(props) {
           <Typography gutterBottom variant="h5" component="h2">
             Expense Type: {props.type}
           </Typography>
-          <Typography gutterBottom variant="h5" component="h2"> 
+          <Typography gutterBottom variant="h5" component="h2">
             Date: {props.date}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Time: {props.time}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/dialog.js
+++ src/dialog.js
@@ -1,42 +1,41 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import { MdShoppingCart } from 'react-icons/md';
-import { GiMilkCarton } from 'react-icons/gi'
-import { FaMobileAlt } from 'react-icons/fa'
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from './card'
-import { AppBar } from '@material-ui/core';
-import NavBar from './appbar';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import { MdShoppingCart } from "react-icons/md";
+import { GiMilkCarton } from "react-icons/gi";
+import { FaMobileAlt } from "react-icons/fa";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "./card";
+import { AppBar } from "@material-ui/core";
+import NavBar from "./appbar";
 
 const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }));
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
-  
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
-  const [age, setAge] = React.useState('');
+  const [age, setAge] = React.useState("");
   const [total1, setTotal] = React.useState(localStorage.getItem("total") || 0);
   // window.addEventListener('storage',function(e){console.log("change");setTotal(localStorage.getItem("total"))})
-  var col="";
+  var col = "";
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -47,50 +46,69 @@ export default function FormDialog() {
   };
 
   function addExpense(props) {
-    let count=1,total=0;
-        if(localStorage.getItem("count"))
-            count = localStorage.getItem("count");
-        else
-            {
-                localStorage.setItem("count",1);
-                localStorage.setItem("expenses",{});
-            }
-        if(localStorage.getItem("total"))
-            total = localStorage.getItem("total");
-        else
-            {
-                localStorage.setItem("total",0);
-            }
+    let count = 1,
+      total = 0;
+    var d = new Date();
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var n = month[d.getMonth()];
+    if (localStorage.getItem("count")) count = localStorage.getItem("count");
+    else {
+      localStorage.setItem("count", 1);
+      localStorage.setItem("expenses", {});
+    }
+    if (localStorage.getItem("total")) total = localStorage.getItem("total");
+    else {
+      localStorage.setItem("total", 0);
+    }
     let expenses = JSON.parse(localStorage.getItem("expenses") || "[]");
-    let element={};
-    element.name=props.name;
-    element.amount=props.amount;
-    element.type=props.type;
-    element.date=Date();
-    total=parseInt(total)+parseInt(props.amount);
-    expenses.push((element));
-    localStorage.setItem("expenses",JSON.stringify(expenses));
-    localStorage.setItem("count",parseInt(count)+1);
-    localStorage.setItem("total",total);
+    let element = {};
+    element.name = props.name;
+    element.amount = props.amount;
+    element.type = props.type;
+    element.date = d.getDate() + " " + n + "," + " " + d.getFullYear();
+    element.time = d.toLocaleTimeString();
+    total = parseInt(total) + parseInt(props.amount);
+    expenses.push(element);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    localStorage.setItem("count", parseInt(count) + 1);
+    localStorage.setItem("total", total);
     setTotal(total);
     handleClose();
   }
 
-  { 
-    let count=1;
-    if(localStorage.getItem("count"))
-        count = localStorage.getItem("count");
-    else
-        {
-            localStorage.setItem("count",1);
-        }
-    let expenses=JSON.parse(localStorage.getItem("expenses") || "[]");
-    var temp1=[];
-    for(let i=0;i<expenses.length;i++)
-    {
-        temp1.push(<Card name={expenses[i].name} amount={expenses[i].amount} type={expenses[i].type} date={expenses[i].date} col={expenses[i].col} />);
+  {
+    let count = 1;
+    if (localStorage.getItem("count")) count = localStorage.getItem("count");
+    else {
+      localStorage.setItem("count", 1);
     }
-  };
+    let expenses = JSON.parse(localStorage.getItem("expenses") || "[]");
+    var temp1 = [];
+    for (let i = 0; i < expenses.length; i++) {
+      temp1.push(
+        <Card
+          name={expenses[i].name}
+          amount={expenses[i].amount}
+          type={expenses[i].type}
+          date={expenses[i].date}
+          time={expenses[i].time}
+          col={expenses[i].col}
+        />
+      );
+    }
+  }
 
   const classes = useStyles();
 
@@ -101,14 +119,20 @@ export default function FormDialog() {
   return (
     <div>
       <NavBar total={total1} />
-        <div id="cards" class="cards">{temp1}</div>
-        
-        <div id="create" onClick={handleClickOpen}>
-                <Fab color="primary" aria-label="add">
-                <AddIcon />
-                </Fab>
-        </div>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <div id="cards" class="cards">
+        {temp1}
+      </div>
+
+      <div id="create" onClick={handleClickOpen}>
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Add Expense</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -129,30 +153,51 @@ export default function FormDialog() {
             type="text"
             fullWidth
           />
-        <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-autowidth-label">Expense Type</InputLabel>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-autowidth-label">
+              Expense Type
+            </InputLabel>
             <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={age}
-            onChange={handleChange}
-            autoWidth
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={age}
+              onChange={handleChange}
+              autoWidth
             >
-            <MenuItem value="">
+              <MenuItem value="">
                 <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}><MdShoppingCart /> Clothing</MenuItem>
-            <MenuItem value={20}><GiMilkCarton /> Eateries</MenuItem>
-            <MenuItem value={30}><FaMobileAlt /> Electronics</MenuItem>
+              </MenuItem>
+              <MenuItem value={10}>
+                <MdShoppingCart /> Clothing
+              </MenuItem>
+              <MenuItem value={20}>
+                <GiMilkCarton /> Eateries
+              </MenuItem>
+              <MenuItem value={30}>
+                <FaMobileAlt /> Electronics
+              </MenuItem>
             </Select>
-      </FormControl>
-   
+          </FormControl>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={(e) => {if(isNaN(document.getElementById("amount").value)){alert("Please enter amount in number!");return;}; addExpense({name:document.getElementById("name").value,amount:document.getElementById("amount").value,type:document.getElementById("demo-simple-select-autowidth").innerText})}} color="primary">
+          <Button
+            onClick={(e) => {
+              if (isNaN(document.getElementById("amount").value)) {
+                alert("Please enter amount in number!");
+                return;
+              }
+              addExpense({
+                name: document.getElementById("name").value,
+                amount: document.getElementById("amount").value,
+                type: document.getElementById("demo-simple-select-autowidth")
+                  .innerText,
+              });
+            }}
+            color="primary"
+          >
             Add Expense
           </Button>
         </DialogActions>
@@ -160,4 +205,3 @@ export default function FormDialog() {
     </div>
   );
 }
-
